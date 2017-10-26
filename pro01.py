0fac0cbb854bbbdc2885991841d44bfb73329fcc
tandavala <tandavala@github.com>
2017-10-26 16:03:46 +0100

    training our chatbot

--- pro01.py
+++ pro01.py
@@ -2,4 +2,22 @@ from chatterbot import ChatBot
 
 chatbot = ChatBot("Ron Obvius")
 
-print(chatbot)
+# training the chatbot
+from chatterbot.trainers import ListTrainer
+
+conversation = [
+	'Hello',
+	'hi there!',
+	'How are you doing?',
+	'I\' doing great.',
+	'That is good to hear',
+	'Thank you',
+	'You\'re welcome.'
+]
+
+chatbot.set_trainer(ListTrainer)
+chatbot.train(conversation)
+
+response = chatbot.get_response("Good morning!")
+
+print(response)
