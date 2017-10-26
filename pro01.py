b2877515a94ecc60ae82a637fa715c24967ed230
tandavala <tandavala@github.com>
2017-10-26 16:11:45 +0100

    making it read only

--- pro01.py
+++ pro01.py
@@ -1,6 +1,6 @@
 from chatterbot import ChatBot
 
-chatbot = ChatBot("Ron Obvius")
+chatbot = ChatBot("Ron Obvius", read_only=True)
 
 # training the chatbot
 from chatterbot.trainers import ListTrainer
@@ -9,7 +9,7 @@ conversation = [
 	'Hello',
 	'hi there!',
 	'How are you doing?',
-	'I\'m doing great.',
+	'I am doing great.',
 	'That is good to hear',
 	'Thank you',
 	'You\'re welcome.'
@@ -21,3 +21,5 @@ chatbot.train(conversation)
 response = chatbot.get_response("Good morning!")
 
 print(response)
+""" Read only mode """
+
